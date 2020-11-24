fb6385ca459d18282d355228e5ef9735a0e4800c
Victor <victor.s@live.dk>
2020-11-24 14:48:54 +0100

    setpoint with seekbar

--- FilipsWho/app/src/main/java/dk/sdu/filipswho/MainActivity.java
+++ FilipsWho/app/src/main/java/dk/sdu/filipswho/MainActivity.java
@@ -9,7 +9,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import static android.hardware.SensorManager.SENSOR_DELAY_NORMAL;
@@ -19,6 +19,7 @@ import nl.stijngroenen.tradfri.device.Gateway;
 import nl.stijngroenen.tradfri.device.Light;
 import nl.stijngroenen.tradfri.device.event.EventHandler;
 import nl.stijngroenen.tradfri.device.event.LightChangeBrightnessEvent;
+import nl.stijngroenen.tradfri.util.ColourHex;
 import nl.stijngroenen.tradfri.util.Credentials;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -33,10 +34,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static final int LIGHTBULB_ID = 65541;
 
     // State variables
-    private int setpoint = 500;
+    private int latestLux = 0;
+    private boolean autoAdjustIsRunning = false;
+    private int setpoint = 0;
     private int setpointError = 15;
-    private int brightness = 0;
-    private int adjustmentLevel = 2;
+    private int desiredBrightness = 0;
+    private int actualBrightness = 0;
+    private int adjustmentLevel = 1;
     private int minBrightness = 1;
     private int maxBrightness = 254;
 
@@ -51,15 +55,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (this.gateway == null) {
             initializeGateway();
         }
+        this.desiredBrightness = this.actualBrightness;
+        SeekBar brightnessBar = findViewById(R.id.brightnessSeekbar);
+        brightnessBar.setProgress(this.actualBrightness);
+        brightnessBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendBrightnessLevel(i);
 
-        initializeUiValues();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                autoAdjustIsRunning = false;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
-    private void initializeUiValues() {
-        // Setpoint
-        EditText setpointInput = findViewById(R.id.inputSetPoint);
-        setpointInput.setText(this.setpoint + "");
-    }
+
 
     private void initializeSensor() {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -74,31 +93,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Device lightDevice = gateway.getDevice(LIGHTBULB_ID);
         this.lightBulb = gateway.getDevice(LIGHTBULB_ID).toLight();
-        setBrightness(this.lightBulb.getBrightness());
+        setActualBrightness(this.lightBulb.getBrightness());
+        this.lightBulb.setColourHex(ColourHex.WARM);
 
         lightDevice.enableObserve();
         lightDevice.addEventHandler(new EventHandler<LightChangeBrightnessEvent>() {
             @Override
             public void handle(LightChangeBrightnessEvent event) {
-                setBrightness(event.getNewBrightness());
+                setActualBrightness(event.getNewBrightness());
                 System.out.println("Received brightness: " + event.getNewBrightness());
             }
         });
     }
 
-    public void setBrightness(int brightness) {
-        this.brightness = brightness;
+    public void setActualBrightness(int actualBrightness) {
+        this.actualBrightness = actualBrightness;
     }
 
     public void btnSetpointHandler(View view) {
-        EditText input = findViewById(R.id.inputSetPoint);
-        this.setpoint = Integer.parseInt(input.getText().toString());
+        SeekBar input = findViewById(R.id.brightnessSeekbar);
+        this.setpoint = latestLux;
+        autoAdjustIsRunning = true;
+        desiredBrightness = actualBrightness;
         System.out.println("New setpoint: " + setpoint);
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         float illuminance = event.values[0];
+        this.latestLux = (int)illuminance;
 
         TextView txtIlluminance = findViewById(R.id.txtIlluminance);
 
@@ -106,29 +130,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         txtIlluminance.setText(illuminance + unit);
 
+        if(this.autoAdjustIsRunning)
         adjustLightSource((int)illuminance, this.setpoint, this.setpointError);
 
     }
 
     private void adjustLightSource(int lux, int setpoint, int setpointError) {
-        int difference = lux - setpoint;
-        int newBrightness;
+        int difference =lux - setpoint;
 
+        int multiplier = 1;
         if (difference > setpointError) {
-            if (this.brightness > this.minBrightness) {
-                 newBrightness = brightness - this.adjustmentLevel;
-                lightBulb.setBrightness(newBrightness);
-                System.out.println("Adjust light source: " + newBrightness);
+            if (this.desiredBrightness > this.minBrightness) {
+                this.desiredBrightness = desiredBrightness - this.adjustmentLevel * multiplier;
             }
         } else if (difference < setpointError * -1) {
-            if (this.brightness < this.maxBrightness) {
-                newBrightness = brightness + this.adjustmentLevel;
-                lightBulb.setBrightness(newBrightness);
-                System.out.println("Adjust light source: " + newBrightness);
+            if (this.desiredBrightness < this.maxBrightness) {
+                this.desiredBrightness = desiredBrightness + this.adjustmentLevel *multiplier;
+
             }
         }
 
 
+        sendBrightnessLevel(this.desiredBrightness);
+
+
+    }
+
+    private void sendBrightnessLevel(int newBrightness) {
+        if(newBrightness < minBrightness)
+            newBrightness = minBrightness;
+        if(newBrightness > maxBrightness)
+            newBrightness = maxBrightness;
+
+        lightBulb.setBrightness(newBrightness);
+        System.out.println("Adjust light source: " + newBrightness);
     }
 
     @Override
