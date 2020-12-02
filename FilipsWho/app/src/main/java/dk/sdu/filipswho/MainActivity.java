4fbbc412f8c48c7cba3f46884187cf5056bff321
Christian Clausen <csbc@mmmi.sdu.dk>
2020-12-02 15:31:45 +0100

    Updated app with new UI, regression watt usage and without event notifications from the bulb

--- FilipsWho/app/src/main/java/dk/sdu/filipswho/MainActivity.java
+++ FilipsWho/app/src/main/java/dk/sdu/filipswho/MainActivity.java
@@ -9,6 +9,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -24,50 +26,65 @@ import nl.stijngroenen.tradfri.util.Credentials;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private TextView txtPrint = null;
     private SensorManager sensorManager;
     private Sensor sensor;
 
+    // Gateway variables
     private Gateway gateway = null;
     private Credentials credentials = null;
     private Light lightBulb = null;
     private static final int LIGHTBULB_ID = 65541;
+    private static final String GATEWAY_IP = "192.168.1.100";
+    private static final String GATEWAY_PASSPHRASE = "mbpRdLgXUNXBzS5M";
 
-    // State variables
+    // Intensity variables
     private int latestLux = 0;
-    private boolean autoAdjustIsRunning = false;
+    private boolean isRunning = false;
     private int setpoint = 0;
-    private int setpointError = 15;
-    private int desiredBrightness = 0;
-    private int actualBrightness = 0;
-    private int adjustmentLevel = 1;
-    private int minBrightness = 1;
-    private int maxBrightness = 254;
+    private int slack = 15;
+    private int desiredIntensity = 0; // The intensity that we wish to set
+    private int actualIntensity = 0; // The actual intensity received from the light bulb
+    private static final int RESOLUTION = 1; // This is the resolution used to adjust the intensity state
+    private static final int MIN_INTENSITY = 1;
+    private static final int MAX_INTENSITY = 254;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtPrint = findViewById(R.id.txtPrint);
 
         initializeSensor();
 
         if (this.gateway == null) {
             initializeGateway();
         }
-        this.desiredBrightness = this.actualBrightness;
-        SeekBar brightnessBar = findViewById(R.id.brightnessSeekbar);
-        brightnessBar.setProgress(this.actualBrightness);
+
+        initializeUiElements();
+    }
+
+    private void initializeUiElements() {
+        // Config section
+        TextView inputSetpoint = findViewById(R.id.inputSetpoint);
+        inputSetpoint.setText(String.valueOf(this.setpoint));
+
+        TextView inputSlack = findViewById(R.id.inputSlack);
+        inputSlack.setText(String.valueOf(this.slack));
+
+        // Seekbar section
+        SeekBar brightnessBar = findViewById(R.id.intensitySeekbar);
+        brightnessBar.setProgress(this.actualIntensity);
         brightnessBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 sendBrightnessLevel(i);
-
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                autoAdjustIsRunning = false;
+
+                if (isRunning) {
+                    btnStartStopHandler(null);
+                }
             }
 
             @Override
@@ -75,9 +92,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
             }
         });
-
     }
-
 
 
     private void initializeSensor() {
@@ -88,34 +103,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     private void initializeGateway() {
-        this.gateway = new Gateway("192.168.1.100");
-        this.credentials = this.gateway.connect("mbpRdLgXUNXBzS5M");
+        this.gateway = new Gateway(GATEWAY_IP);
+        this.credentials = this.gateway.connect(GATEWAY_PASSPHRASE);
 
-        Device lightDevice = gateway.getDevice(LIGHTBULB_ID);
         this.lightBulb = gateway.getDevice(LIGHTBULB_ID).toLight();
-        setActualBrightness(this.lightBulb.getBrightness());
+        this.actualIntensity = this.lightBulb.getBrightness();
         this.lightBulb.setColourHex(ColourHex.WARM);
-
-        lightDevice.enableObserve();
-        lightDevice.addEventHandler(new EventHandler<LightChangeBrightnessEvent>() {
-            @Override
-            public void handle(LightChangeBrightnessEvent event) {
-                setActualBrightness(event.getNewBrightness());
-                System.out.println("Received brightness: " + event.getNewBrightness());
-            }
-        });
     }
 
-    public void setActualBrightness(int actualBrightness) {
-        this.actualBrightness = actualBrightness;
-    }
+    public void btnStartStopHandler(View view) {
 
-    public void btnSetpointHandler(View view) {
-        SeekBar input = findViewById(R.id.brightnessSeekbar);
+        Button btnStartStop = findViewById(R.id.btnStartStop);
+        EditText inputSlack = findViewById(R.id.inputSlack);
+        TextView txtRunning = findViewById(R.id.txtRunningOut);
+
+        // Update state variables
         this.setpoint = latestLux;
-        autoAdjustIsRunning = true;
-        desiredBrightness = actualBrightness;
-        System.out.println("New setpoint: " + setpoint);
+        desiredIntensity = actualIntensity;
+
+        if (isRunning) {
+            // Update state variables
+            isRunning = false;
+
+            // Disable and update UI elements
+            btnStartStop.setText("Start");
+            inputSlack.setEnabled(true);
+            txtRunning.setText("False");
+
+        } else {
+            isRunning = true;
+
+            // Update System Status View
+            TextView inputSetpoint = findViewById(R.id.inputSetpoint);
+            inputSetpoint.setText(String.valueOf(this.latestLux));
+            txtRunning.setText("True");
+
+            // Disable and update UI elements
+            this.slack = Integer.parseInt(inputSlack.getText().toString());
+            inputSlack.setEnabled(false);
+
+            btnStartStop.setText("Stop");
+        }
+
+
     }
 
 
@@ -124,15 +154,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float illuminance = event.values[0];
         this.latestLux = (int)illuminance;
 
-        TextView txtIlluminance = findViewById(R.id.txtIlluminance);
-
+        // Update lux in UI
+        TextView txtIlluminance = findViewById(R.id.txtCurrentLuxOut);
         String unit = " lx";
-
         txtIlluminance.setText(illuminance + unit);
 
-        if(this.autoAdjustIsRunning)
-        adjustLightSource((int)illuminance, this.setpoint, this.setpointError);
+        // Calculate the Slack error
+        TextView txtSlackErrorOut = findViewById(R.id.txtSlackErrorOut);
+        txtSlackErrorOut.setText(Math.abs(setpoint - latestLux) + unit);
 
+        // Run light adjustment
+        if(this.isRunning) {
+            adjustLightSource(latestLux, this.setpoint, this.slack);
+        }
     }
 
     private void adjustLightSource(int lux, int setpoint, int setpointError) {
@@ -140,30 +174,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         int multiplier = 1;
         if (difference > setpointError) {
-            if (this.desiredBrightness > this.minBrightness) {
-                this.desiredBrightness = desiredBrightness - this.adjustmentLevel * multiplier;
+            if (this.desiredIntensity > this.MIN_INTENSITY) {
+                this.desiredIntensity = desiredIntensity - this.RESOLUTION * multiplier;
+                sendBrightnessLevel(this.desiredIntensity);
             }
         } else if (difference < setpointError * -1) {
-            if (this.desiredBrightness < this.maxBrightness) {
-                this.desiredBrightness = desiredBrightness + this.adjustmentLevel *multiplier;
-
+            if (this.desiredIntensity < this.MAX_INTENSITY) {
+                this.desiredIntensity = desiredIntensity + this.RESOLUTION *multiplier;
+                sendBrightnessLevel(this.desiredIntensity);
             }
         }
-
-
-        sendBrightnessLevel(this.desiredBrightness);
-
-
     }
 
-    private void sendBrightnessLevel(int newBrightness) {
-        if(newBrightness < minBrightness)
-            newBrightness = minBrightness;
-        if(newBrightness > maxBrightness)
-            newBrightness = maxBrightness;
+    private void sendBrightnessLevel(int newIntensity) {
+        if(newIntensity < MIN_INTENSITY)
+            newIntensity = MIN_INTENSITY;
+        if(newIntensity > MAX_INTENSITY)
+            newIntensity = MAX_INTENSITY;
 
-        lightBulb.setBrightness(newBrightness);
-        System.out.println("Adjust light source: " + newBrightness);
+        lightBulb.setBrightness(newIntensity);
+
+        // Update UI element
+        TextView txtIntensity = findViewById(R.id.txtIntensityOut);
+        txtIntensity.setText(String.valueOf(newIntensity));
+
+        TextView txtWattOut = findViewById(R.id.txtWattOut);
+        txtWattOut.setText(String.valueOf(round(wattUsage(newIntensity), 2)));
+
+        SeekBar intensitySeekBar = findViewById(R.id.intensitySeekbar);
+        intensitySeekBar.setProgress(newIntensity, true);
+    }
+
+    /**
+     * This method is based on a regression model made with a watt meter and an IKEA E27 1000 lumen light bulb
+     * @param intensity Value between 0 and 254
+     * @return the watt usage from 0.2 watt to 11.8 watt
+     */
+    private double wattUsage(int intensity) {
+        if (intensity == 0){
+            return 0.2;
+        } else {
+            return 0.737514880516844 * Math.exp(0.010723316391607 * intensity);
+        }
+    }
+
+    /**
+     * Returns the provided double value to the amount of decimals
+     * @param d
+     * @param decimals
+     * @return
+     */
+    private double round(double d, int decimals) {
+        return  Math.round(d * Math.pow(10, decimals)) / Math.pow(10, decimals);
     }
 
     @Override
